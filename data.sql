7543fba9fb32b5d55bb7ac53e4fafdb359355d44
koppai <psthu.koppai@gmail.com>
2023-08-16 22:21:17 +0630

    Insert datas

--- data.sql
+++ data.sql
@@ -1,14 +1,126 @@
 /* Populate database with sample data. */
+INSERT INTO
+  animals
+VALUES
+  (1, 'Agumon', '2020-02-03', 0, true, 10.23);
 
-INSERT INTO animals
-VALUES (1,'Agumon','2020-02-03',0,true,10.23);
-INSERT INTO animals
-VALUES (2,'Gabumon','2018-11-15',2,true,8);
-INSERT INTO animals
-VALUES (3,'Pikachu','2021-01-07',1,false,15.04);
-INSERT INTO animals
-VALUES (4,'Devimon','2017-05-12',5,true,11);
+INSERT INTO
+  animals
+VALUES
+  (2, 'Gabumon', '2018-11-15', 2, true, 8);
+
+INSERT INTO
+  animals
+VALUES
+  (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
+
+INSERT INTO
+  animals
+VALUES
+  (4, 'Devimon', '2017-05-12', 5, true, 11);
 
 -- QUWERY AND UPDATE ANIMALS TABLE
-INSERT INTO animals
-VALUES (5, 'Charmander', '2020-02-08', 0, false, -11.0),(6, 'Plantmon', '2021-11-15', 2, true, -5.7),(7, 'Squirtle', '1993-04-02', 3, false, -12.13),(8, 'Angemon','2005-06-12', 1, true, -45),(9, 'Boarmon','2005-06-07', 7, true, 20.4),(10, 'Blossom','1998-10-13', 3, true, 17),(11, 'Ditto','2022-05-14', 4, true, 22);
+INSERT INTO
+  animals
+VALUES
+  (5, 'Charmander', '2020-02-08', 0, false, -11.0),
+  (6, 'Plantmon', '2021-11-15', 2, true, -5.7),
+  (7, 'Squirtle', '1993-04-02', 3, false, -12.13),
+  (8, 'Angemon', '2005-06-12', 1, true, -45),
+  (9, 'Boarmon', '2005-06-07', 7, true, 20.4),
+  (10, 'Blossom', '1998-10-13', 3, true, 17),
+  (11, 'Ditto', '2022-05-14', 4, true, 22);
+
+-- QUWERY MULTIPLE TABLES
+INSERT INTO
+  owners (full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+INSERT INTO
+  species (name)
+VALUES
+  ('Pokemon'),
+  ('Digimon');
+
+UPDATE animals
+SET
+  species_id = 2
+WHERE
+  name LIKE '%mon';
+
+UPDATE animals
+SET
+  species_id = 1
+WHERE
+  name NOT LIKE '%mon';
+
+UPDATE animals
+SET
+  owner_id = (
+    SELECT
+      id
+    from
+      owners
+    where
+      full_name = 'Sam Smith'
+  )
+WHERE
+  name = 'Agumon';
+
+UPDATE animals
+SET
+  owner_id = (
+    SELECT
+      id
+    from
+      owners
+    where
+      full_name = 'Jennifer Orwell'
+  )
+WHERE
+  name in ('Pikachu', 'Gabumon');
+
+UPDATE animals
+SET
+  owner_id = (
+    SELECT
+      id
+    from
+      owners
+    where
+      full_name = 'Bob'
+  )
+WHERE
+  name in ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET
+  owner_id = (
+    SELECT
+      id
+    from
+      owners
+    where
+      full_name = 'Melody Pond'
+  )
+WHERE
+  name in ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET
+  owner_id = (
+    SELECT
+      id
+    from
+      owners
+    where
+      full_name = 'Dean Winchester'
+  )
+WHERE
+  name in ('Angemon', 'Boarmon');
